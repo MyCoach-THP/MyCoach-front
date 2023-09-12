8baf30bac85804691087f0e0e625195e41f0246b
Antoine Paturel <antoine.paturel34@free.fr>
2023-09-12 17:01:56 +0200

    responsivity description + scss

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -2,28 +2,35 @@ import React from 'react';
 
 const Description = () => {
   return (
-    <div style={{ width: '100%', color: 'black', textAlign: 'center', fontSize: '20px' }}>
+    <div className="description-container">
 
       <h1 className='description-title'>
         Bienvenue
       </h1>
 
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: '10vh', marginRight: '10vh' }}>
+      <p className="description-text">
+        Êtes-vous prêt à transformer votre vie sportive et à atteindre vos objectifs de forme physique?
+      </p>
 
-        <img
-          src="https://res.cloudinary.com/do3pr1kdp/image/upload/v1694527566/Mycoach/onhbpybwcs5tv7l0shaj.png"
-          alt="Coach"
-          style={{ width: '30vh', height: 'auto', marginRight: '5vh', marginLeft: '3vh' }}
-        />
+      <p className="description-highlight">
+        <strong><span className="description-blue">My</span>Coach</strong> est la plateforme qu'il vous faut!
+      </p>
 
-        <div>
-          <p>
-            Êtes-vous prêt à transformer votre vie sportive et à atteindre vos objectifs de forme physique? <strong><span style={{ color: '#00ccff' }}> My</span>Coach</strong> est la plateforme qu'il vous faut! Avec une vaste sélection de coachs, des programmes adaptés à tous les niveaux et un soutien constant, nous sommes ici pour vous guider à chaque étape. Plus qu'une simple plateforme, MyCoach est une communauté où chaque membre est valorisé, soutenu et motivé à exceller. Ne laissez pas passer cette chance ! Rejoignez-nous dès aujourd'hui et découvrez le meilleur de vous-même.
-          </p>
-        </div>
+      <img
+        src="https://res.cloudinary.com/do3pr1kdp/image/upload/v1694527566/Mycoach/onhbpybwcs5tv7l0shaj.png"
+        alt="Coach"
+        className="description-image"
+      />
 
-      </div>
-
+      <p className="description-text">
+        Avec une vaste sélection de coachs, des programmes adaptés à tous les niveaux et un soutien constant, nous sommes ici pour vous guider à chaque étape.
+      </p>
+      <p className="description-text-small">
+        Plus qu'une simple plateforme, MyCoach est une communauté où chaque membre est valorisé, soutenu et motivé à exceller.
+      </p>
+      <p className="description-call-to-action">
+        Ne laissez pas passer cette chance ! Rejoignez-nous dès aujourd'hui et découvrez le meilleur de vous-même.
+      </p>
 
     </div >
   );
