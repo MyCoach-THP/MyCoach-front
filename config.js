0f2981830835ad957dbda480359fc940df7341db
Guillaume Regimbeau <gregimbeau@mail.com>
2023-09-18 15:19:42 +0200

    vercel+404

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-export const API_BASE_URL = "http://localhost:3000";
-// export const API_BASE_URL = "https://mycoach.fly.dev";
-// export const API_BASE_URL = "https://well-worn-photo-may.domcloud.io/";
+// export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "https://mycoach.fly.dev";
